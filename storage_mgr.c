d56945423d697da82100eee1da129246a9c2cbd5
parkerjoncus <parkerj@me.com>
2018-02-09 14:57:13 -0600

    Create Page File method

--- storage_mgr.c
+++ storage_mgr.c
@@ -1,20 +1,29 @@
-//
-//  storage_mgr.c
-//  
-//
-//  Created by Parker Joncus on 2/9/18.
-//
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#inslude <string.h>
 #include "storage_mgr.h"
+#include "dberror.h"
 
-void
+void initStorageManager (void){
+    
+}
 
 RC createPageFile (char *filename){
     FILE *fpointer;
     fpointer = fopen(filename,"w");
-    
+    if (fp==NULL){
+        return RC_FILE_NOT_FOUND;
+    }
+    SM_PageHandle newpage = (SM_PageHandle) calloc(PAGE_SIZE, size(char));
+    int writePage = fwrite(newpage, sizeof(char), PAGE_SIZE, fpointer);
+    if (writePage==0){
+        return RC_WRITE_FAILED
+    }
+    free(newpage)
     fclose(fpointer);
-    return RC_OK
+    return RC_OK;
 }
 
 RC openPageFile(char *filename, ){
