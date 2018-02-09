c87522b449d7af60983e598db1a1b37451b91c01
Parker Joncus <parkerjoncus@dhcp4.merunorth104.iit.edu>
2018-02-09 14:33:56 -0600

    methods outline

--- storage_mgr.c
+++ storage_mgr.c
@@ -6,3 +6,25 @@
 //
 
 #include "storage_mgr.h"
+
+void
+
+RC createPageFile (char *filename){
+    FILE *fpointer;
+    fpointer = fopen(filename,"w");
+    
+    fclose(fpointer);
+    return RC_OK
+}
+
+RC openPageFile(char *filename, ){
+    
+}
+
+RC closePageFile (){
+    
+}
+
+RC destroyPageFile (char *filename){
+    
+}
