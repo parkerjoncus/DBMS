7f01e03f55aeac7760173e4ae34186c537357f81
Parker Joncus <parkerjoncus@dhcp4.merunorth104.iit.edu>
2018-02-09 15:45:51 -0600

    close PF and destroy PF

--- storage_mgr.c
+++ storage_mgr.c
@@ -12,28 +12,39 @@ void initStorageManager (void){
 
 RC createPageFile (char *filename){
     FILE *fpointer;
-    fpointer = fopen(filename,"w");
+    fpointer = fopen(filename,"wb");
     if (fp==NULL){
         return RC_FILE_NOT_FOUND;
     }
     SM_PageHandle newpage = (SM_PageHandle) calloc(PAGE_SIZE, size(char));
     int writePage = fwrite(newpage, sizeof(char), PAGE_SIZE, fpointer);
     if (writePage==0){
-        return RC_WRITE_FAILED
+        return RC_WRITE_FAILED;
     }
-    free(newpage)
+    free(newpage);
     fclose(fpointer);
     return RC_OK;
 }
 
-RC openPageFile(char *filename, ){
+RC openPageFile(char *filename, SM_FileHandle *fHandle){
+    FILE *fpointer;
+    fpointer = fopen(*filename,"r+b");
+    if (fpointer==NULL){
+        return RC_FILE_NOT_FOUND
+    }
     
 }
 
-RC closePageFile (){
-    
+RC closePageFile (SM_FileHandle *fHandle){
+    if(fclose(*fHandle->mgmtInfo)==0){
+        return RC_OK
+    }
+    return RC_FILE_NOT_FOUND
 }
 
 RC destroyPageFile (char *filename){
-    
+    if(remove(filename)==0){
+        return RC_OK
+    }
+    return RC_FILE_NOT_FOUND
 }
