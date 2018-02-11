b1eeefea76e972ed533c7a9a31ac6eae498922af
Yfallourd <yohann.fallourd@gmail.com>
2018-02-11 14:50:23 -0600

    Read methods done

--- storage_mgr.c
+++ storage_mgr.c
@@ -1,8 +1,41 @@
-//
-//  storage_mgr.c
-//  
-//
-//  Created by Parker Joncus on 2/9/18.
-//
-
 #include "storage_mgr.h"
+
+RC readBlock(int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage) {
+    if (fHandle->totalNumPages < pageNum) {
+        return RC_READ_NON_EXISTING_PAGE;
+    }
+    //Set stream in POSIX file descriptor to the pageNumth page
+    fseek(fHandle->mgmtInfo, sizeof(char) * PAGE_SIZE * pageNum, SEEK_SET);
+
+    //Read PAGE_SIZE chars from the stream char by char, result in memPage
+    fread(memPage, sizeof(char), PAGE_SIZE, fHandle->mgmtInfo);
+
+    //Update the fileHandle
+    fHandle->curPagePos = pageNum;
+
+    return RC_OK;
+}
+
+int getBlockPos(SM_FileHandle *fHandle) {
+    return fHandle->curPagePos;
+}
+
+RC readFirstBlock(SM_FileHandle *fHandle, SM_PageHandle memPage) {
+    return readBlock(0, fHandle, memPage);
+}
+
+RC readPreviousBlock(SM_FileHandle *fHandle, SM_PageHandle memPage) {
+    return readBlock(fHandle->curPagePos - 1, fHandle, memPage);
+}
+
+RC readCurrentBlock(SM_FileHandle *fHandle, SM_PageHandle memPage) {
+    return readBlock(fHandle->curPagePos, fHandle, memPage);
+}
+
+RC readNextBlock(SM_FileHandle *fHandle, SM_PageHandle memPage) {
+    return readBlock(fHandle->curPagePos + 1, fHandle, memPage);
+}
+
+RC readLastBlock(SM_FileHandle *fHandle, SM_PageHandle memPage) {
+    return readBlock(fHandle->totalNumPages - 1, fHandle, memPage);
+}
