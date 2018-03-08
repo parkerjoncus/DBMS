008749c74c107f6d1f83e90f293dfe598ed7898b
Parker Joncus <parkerjoncus@dhcp128.merunorth97.iit.edu>
2018-03-07 20:38:18 -0600

    initial work outline

--- Assignment2/buffer_mgr.c
+++ Assignment2/buffer_mgr.c
@@ -1,8 +1,31 @@
-//
-//  buffer_mgr.c
-//  
-//
-//  Created by Parker Joncus on 3/7/18.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#include "buffer_mgr.h"
+#include "dberror.h"
+#include "storage_mgr.h"
+#include "dt.h"
+
+//initialize the Buffer Pool
+RC initBufferPool(BM_BufferPool *const bm, const char *const pageFileName,
+                  const int numPages, ReplacementStratetgy strategy,
+                  void *stratData){
+    
+    bm->pageFile = (char*) pageFileName;
+    bm->numPages= numPages;
+    bm->strategy = strategy;
+    
+    
+    return RC_OK
+}
+
+//Shut down the Buffer Pool
+RC shutdownBufferPool(BM_BufferPool *const bm){
+    return RC_OK
+}
+
+//Forse
+RC forceFlushPool(BM_bufferPool *const bm){
+    return RC_OK
+}
